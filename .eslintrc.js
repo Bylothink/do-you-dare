f80d2bf240377890a0ac2d497d1e12f3aad24fd0
Matteo Bilotta <me@byloth.net>
2021-06-23 22:03:09 +0200

    [FIX] Corretto stile progetto.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,17 @@
 module.exports = {
   root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
+  extends: [
+    "@byloth/eslint-config-typescript",
+    "@byloth/eslint-config-vue"
   ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+  overrides: [
+    {
+      files: ["*.ts"],
+      rules: { "no-unused-vars": "off" }
+    },
+    {
+      files: ["*.vue"],
+      rules: { "vue/no-multiple-template-root": "off" }
+    }
+  ]
+};
