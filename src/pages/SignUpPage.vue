2430736f139587cb2e72ea4104e22db0b92e09b6
Rodrigo De Angelis <rodrigodeangelis1@gmail.com>
2021-10-27 12:43:29 +0200

    Form di registrazione corretto e modificato

--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -1,38 +1,68 @@
 /* eslint-disable camelcase */
 <template>
     <CenteredLayout id="sign-up-page" tag="main">
-        <h1>This is a sign up page</h1>
+        <h1>Do you dare?</h1>
         <form @submit.prevent="onSubmit">
             <fieldset>
                 <legend>Registrati</legend>
-                <label>
-                    Nome:
-                    <input v-model="firstName" type="text" />
-                </label>
-                <br />
-                <label>
-                    Cognome:
-                    <input v-model="lastName" type="text" />
-                </label>
-                <br />
-                <label>
-                    Username:
-                    <input v-model="username" type="text" />
-                </label>
-                <br />
-                <label>
-                    Email:
-                    <input v-model="email" type="text" />
-                </label>
-                <br />
-                <label>
-                    Password:
-                    <input v-model="password" type="password" />
-                </label>
+                <div class="row mb-3">
+                    <label for="fName" class="col-sm-3 col-form-label col-form-label-lg">Nome</label>
+                    <div class="col-sm-9">
+                        <input id="fName"
+                               v-model="firstName"
+                               type="text"
+                               class="form-control form-control-lg"
+                               required />
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="lName" class="col-sm-3 col-form-label col-form-label-lg">Cognome</label>
+                    <div class="col-sm-9">
+                        <input id="lName"
+                               v-model="lastName"
+                               type="text"
+                               class="form-control form-control-lg"
+                               required />
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="user" class="col-sm-3 col-form-label col-form-label-lg">Username</label>
+                    <div class="col-sm-9">
+                        <input id="user"
+                               v-model="username"
+                               type="text"
+                               class="form-control form-control-lg"
+                               required />
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="userEmail" class="col-sm-3 col-form-label col-form-label-lg">Email</label>
+                    <div class="col-sm-9">
+                        <input id="userEmail"
+                               v-model="email"
+                               type="email"
+                               class="form-control form-control-lg"
+                               required />
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="pw" class="col-sm-3 col-form-label col-form-label-lg">Password</label>
+                    <div class="col-sm-9">
+                        <input id="pw"
+                               v-model="password"
+                               type="password"
+                               class="form-control form-control-lg"
+                               required />
+                    </div>
+                </div>
                 <hr />
-                <button type="submit">
-                    Vai
-                </button>
+                <div class="row mb-3 mt-4">
+                    <div class="col-sm-9 offset-sm-3">
+                        <button class="form control form-control-lg btn btn-primary" type="submit">
+                            Conferma
+                        </button>
+                    </div>
+                </div>
             </fieldset>
         </form>
     </CenteredLayout>
@@ -72,4 +102,11 @@
 </script>
 
 <style lang="scss" scoped>
+    #sing-up-page
+    {
+        .btn.btn-primary
+        {
+            width: 100%;
+        }
+    }
 </style>
