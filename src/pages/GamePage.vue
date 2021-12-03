3cd90b0912561d3fe01c5e47cf8c0aee9a45296d
Matteo Bilotta <me@byloth.net>
2021-12-03 18:58:11 +0100

    [FIX] Corretta l'animazione di apertura della home page.

--- src/pages/GamePage.vue
+++ src/pages/GamePage.vue
@@ -41,31 +41,12 @@
 </script>
 
 <style lang="scss" scoped>
-    @keyframes fade-in
-    {
-        0% { margin-bottom: -211px; opacity: 0; }
-        50% { margin-bottom: -211px; }
-        75% { opacity: 0; }
-        100% { margin-bottom: 0px; opacity: 1; }
-    }
-    @keyframes slide-down
-    {
-        0% { margin-top: -211px; }
-        50% { margin-top: -211px; }
-        100% { margin-top: 0.5em; }
-    }
-
     #game-page
     {
         overflow: hidden;
 
-        & > .deck
-        {
-            animation: fade-in 2000ms ease-in-out;
-        }
         & > .title
         {
-            animation: slide-down 2000ms ease-in-out;
             font-size: 6.666em;
             font-weight: 100;
             margin: 0px;
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -53,8 +53,44 @@
 <style lang="scss" scoped>
     @use "@/assets/scss/variables";
 
+    @keyframes slide-down
+    {
+        0% { transform: translateY(137px); }
+        50% { transform: translateY(137px); }
+        100% { transform: translateY(0px); }
+    }
+
+    @keyframes first-fade-in
+    {
+        0% { transform: translateY(-73px); opacity: 0; }
+        50% { transform: translateY(-73px); }
+        75% { opacity: 0; }
+        100% { transform: translateY(0px); opacity: 1; }
+    }
+    @keyframes second-fade-in
+    {
+        0% { transform: translateY(-73px); opacity: 0; }
+        50% { transform: translateY(-73px); }
+        75% { opacity: 0; }
+        100% { transform: translateY(0px); opacity: 1; }
+    }
+
+    @keyframes enter-right
+    {
+        0% { transform: translateX(200%); }
+        75% { transform: translateX(200%); }
+        100% { transform: translateX(0px); }
+    }
+    @keyframes enter-left
+    {
+        0% { transform: translateX(-200%); }
+        75% { transform: translateX(-200%); }
+        100% { transform: translateX(0px); }
+    }
+
     #home-page
     {
+        overflow: hidden;
         text-align: center;
 
         & > ul
@@ -72,6 +108,7 @@
 
         & > .title
         {
+            animation: slide-down 2000ms ease-in-out;
             font-size: 6.666em;
             font-weight: 100;
             margin: 0px;
@@ -87,8 +124,13 @@
         }
         & > #play-now-btn
         {
+            animation: first-fade-in 2000ms ease-in-out;
             font-size: 2.5em;
             margin-bottom: 1em;
+        }
+        & > #game-modes-btn
+        {
+            animation: second-fade-in 2200ms ease-in-out;
         }
 
         & > #settings-btn,
@@ -100,10 +142,12 @@
         }
         & > #settings-btn
         {
+            animation: enter-left 2600ms ease-in-out;
             left: 1em;
         }
         & > #user-btn
         {
+            animation: enter-right 2400ms ease-in-out;
             right: 1em;
         }
     }
