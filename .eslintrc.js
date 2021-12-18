d152a4d9c3a85b95d846ecda88d744e9441eecd7
Matteo Bilotta <mbilotta@linkeurope.it>
2021-12-18 13:32:39 +0100

    [INIT] Initialized project using Vite.js.

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  extends: [
+    "@byloth/eslint-config-typescript",
+    "@byloth/eslint-config-vue"
+  ],
+  globals: {
+    defineProps: true,
+    defineEmits: true,
+    defineExpose: true
+  },
+  overrides: [
+    {
+      files: ["*.ts"],
+      rules: { "no-unused-vars": "off" }
+    }
+  ]
+};
