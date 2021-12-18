f09de9b6fb89938357df794b3ae412132844a7e8
Matteo Bilotta <mbilotta@linkeurope.it>
2021-12-18 15:42:26 +0100

    [ADD] Added some dependencies.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -35,20 +35,21 @@
     </p>
 </template>
 
-<style scoped>
-a {
-  color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
+<style lang="scss" scoped>
+    a
+    {
+        color: #42b983;
+    }
+    label
+    {
+        margin: 0 0.5em;
+        font-weight: bold;
+    }
+    code
+    {
+        background-color: #eee;
+        padding: 2px 4px;
+        border-radius: 4px;
+        color: #304455;
+    }
 </style>
