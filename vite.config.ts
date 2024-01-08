eaf7551133e25f247a0a3030349e2b5b923b4cdc
Matteo Bilotta <me@byloth.dev>
2024-01-08 15:37:20 +0100

    upd: Updated project configs based on Vue 3.4 template.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
-import { resolve } from "path";
 
 import Vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   resolve: {
-    alias: { "@": resolve(__dirname, "src") }
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
   },
   plugins: [Vue()]
 });
